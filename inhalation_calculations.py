cc914f2aee0df332fc09fcaf8593c3df933afada
SeaFeld <92348695+SeaFeld@users.noreply.github.com>
2025-03-01 23:03:52 +0000

    First commit

---
+++ inhalation_calculations.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import streamlit as st
+
+def calculation_inhalation_data(zip, home_hrs, office_hrs, outdoor_hrs):
+    df = pd.read_excel('Microplastics Data.xlsx', sheet_name='App Research - US County Info')
+    for x,y in df.iterrows():
+        zip_list = str(y['zips'])
+        if str(zip) in zip_list:
+            home_exposure =float(y['Estimated home air inhalation per hour'])*home_hrs
+            office_exposure = float(y['Estimated office air inhalation per hour'])*office_hrs
+            outdoor_exposure = float(y['Estimated outdoor air inhalation per hour'])*outdoor_hrs
+            total_inhalation = round(home_exposure+office_exposure+outdoor_exposure,2)
+    return (total_inhalation)
+
+
+
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,80 @@
 import streamlit as st
+import inhalation_calculations
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.set_page_config(
+    page_title="Deplastico Microplastics calculator",
+    page_icon=":desktop_computer:",
+    layout="wide",
+    initial_sidebar_state="expanded",
 )
+
+st.sidebar.header("About")
+
+#st.sidebar.image("logo.png")
+
+st.sidebar.title("FAQ")
+
+st.sidebar.title("Sources")
+
+# ------------------ Main App UI ------------------ #
+
+st.markdown("""
+Complete the fields below to calculate your estimated microplastic exposure
+""")
+st.markdown("""---""")
+
+# Two column layout for the main app content
+col1, col2 = st.columns([1, 1])
+
+# Create input fields for additional details
+with col1:
+    sex = st.selectbox(
+        label="Select your sex",
+        options=[
+            "Male",
+            "Female"
+        ],
+        key="sex",
+    )
+
+    age = st.number_input(
+        label="Enter your age",
+        min_value=0,
+        max_value=110,
+        key="age",
+    )
+# Create input fields for internet_facing and authentication
+    zip_code = st.number_input(
+        label="Enter your zip code",
+        min_value=51,
+        max_value= 999999,
+        key="zip_code",
+    )
+
+    home_hours = st.slider(
+        "How many hours a day do you typically spend at home?",
+        min_value=0,
+        max_value=24,
+        value=12,
+        key="home_hours",
+    )
+
+    office_hours = st.slider(
+        "How many hours a day do you typically spend at in an office?",
+        min_value=0,
+        max_value=24,
+        value=8,
+        key="office_hours",
+    )
+
+    outdoor_hours = st.slider(
+        "How many hours a day do you typically spend at in an outdoor environment?",
+        min_value=0,
+        max_value=24,
+        value=4,
+        key="outdoor_hours",
+    )
+    st.markdown("""---""")
+    st.header("Estimated exposure")
+    st.title(inhalation_calculations.calculation_inhalation_data(zip_code,home_hours,office_hours,outdoor_hours))
+    st.caption('mnp')
