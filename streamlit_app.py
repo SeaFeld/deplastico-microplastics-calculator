f1dd7505f772f7eeab34f73cecf61fb9698cbe80
SeaFeld <92348695+SeaFeld@users.noreply.github.com>
2025-03-01 23:25:20 +0000

    hide running display again

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ hide_streamlit_style = """
                 }
                 </style>
                 """
-#st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
 st.sidebar.header("Welcome")
 
