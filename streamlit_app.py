1c17db9a8b0f0e9b332f62131bafa4faa906bc7c
SeaFeld <92348695+SeaFeld@users.noreply.github.com>
2025-03-02 00:05:18 +0000

    collapsed sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.set_page_config(
     page_title="Deplastico Microplastics calculator",
     page_icon=":desktop_computer:",
     layout="wide",
-    initial_sidebar_state="expanded",
+    initial_sidebar_state="Collapsed",
 )
 
 hide_streamlit_style = """
